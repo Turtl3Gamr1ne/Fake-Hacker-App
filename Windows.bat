c0455be94f38d4d59c2cbd07687039655432428d
TUrtleGamr1ne <bisasam512@gmail.com>
2024-07-06 21:34:00 +0200

    Update Windows.bat

--- Windows.bat
+++ Windows.bat
@@ -10,6 +10,9 @@ cd C:
 ping www.google.com
 ping www.google.com
 ping www.google.com
+start https://www.youtube.com/watch?v=xvFZjo5PgG0
+echo press any key to start hacking!
+pause
 :loop
 dir
 dir
